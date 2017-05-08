e48a0fcc56fb22cf332cf64235c4986f98cf8082
mahoney16 <kevinmahoney@Kevins-MacBook-Pro-2.local>
2017-05-08 18:32:31 -0500

    2DVec.cpp

---
+++ curses-tanks-m/curses-tanks-m/2DVec.cpp
@@ -0,0 +1,35 @@
+//
+//  2DVec.cpp
+//  curses-tanks-m
+//
+//  Created by Kevin Mahoney on 5/8/17.
+//  Copyright © 2017 Carthage College. All rights reserved.
+//
+
+#include "2DVec.hpp"
+
+
+Vec2D :: operator + (const Vec2D & other)
+{
+    Vec2D v;
+    double x;
+    double y;
+    
+
+    v.x = x + other.x;
+    v.y = y + other.y;
+ 
+}
+Vec2D :: operator * (const Vec2D & other)
+{
+    Vec2D v;
+    double x;
+    double y;
+    
+    
+    v.x = x * other.x;
+    v.y = y * other.y;
+    
+}
+
+
